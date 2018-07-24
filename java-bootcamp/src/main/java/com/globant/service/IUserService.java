2012ebc6752ff7938aaeb1805d4200b1d454eaf2
Herminio Andrés Hernández <hherminioandres@gmail.com>
2018-07-23 23:24:57 -0300

    Topic 3: Several corrections
    -Unnecessary comments deleted
    -Typo errors were corrected

--- java-bootcamp/src/main/java/com/globant/service/IUserService.java
+++ java-bootcamp/src/main/java/com/globant/service/IUserService.java
@@ -1,8 +1,8 @@
 package com.globant.service;
 
 /**
- * IUserService defines headders of user service.
- * This class Defines basics operations to implement such as Create, Read, Update and Delete user.
+ * IUserService defines headers of user service.
+ * This class defines basics operations to implement such as Create, Read, Update and Delete user.
  * @author Herminio Andres Hernandez
  * @version 21/07/2018
  */
@@ -10,29 +10,28 @@ package com.globant.service;
 public interface IUserService {
 	
 	/**
-	 * This method defines headder to create a user.
-	 * @param nickname User's nickname.
+	 * This method defines header to create a user.
+	 * @param nickname User's nickname. The nick is used to create the user.
 	 * @return User to create.
 	 */
 	public User create(String nickname);
 	/**
-	 * This method defines headder to read or obtain a user.
-	 * @param nickname User's nickname.
+	 * This method defines header to read or obtain a user.
+	 * @param nickname User's nickname. This is used to search the required user.
 	 * @return User to obtain.
 	 */
 	public User read(String nickname);
 	/**
-	 * This method defines headder to update a user. 
-	 * @param oldNickname User's actual or old nickname.
+	 * This method defines header to update a user. 
+	 * @param oldNickname User's actual or old nickname. This is used to search the user to update.
 	 * @param user User to update.
 	 * @return User to update.
 	 */
 	public User update(String oldNickname, User user);
 	/**
-	 * This method defines headder to delete a user.  
-	 * @param nickname User's nickname.
+	 * This method defines header to delete a user.  
+	 * @param nickname User's nickname. This is used to search the user to delete.
 	 * @return Boolean value of operation delete.
 	 */
 	public boolean delete(String nickname);
-	
 }
--- java-bootcamp/src/main/java/com/globant/service/UserService.java
+++ java-bootcamp/src/main/java/com/globant/service/UserService.java
@@ -1,51 +1,32 @@
 package com.globant.service;
 
 /**
- * "UserService" defines behavior a user service, implements to "IUserService".
- * Implements basics operations such as Create, Read, Update and Delete user.
+ * "UserService" implements to "IUserService".
  * @author Herminio Andres Hernandez
  * @version 21/07/2018
  */
 
 public class UserService implements IUserService{
-	//Attributes of the class.
+
 	private UserRepository userRepository;
-	/**
-	 * Constructor of user service.
-	 */
+
 	public UserService() {
 		userRepository = new UserRepository();
-	}//Close constructor
-	/**
-	 * The method allows to create user. 
-	 * @param nickname User's nickname. The nick is used to create the user.
-	 * @return Instance of the created user.
-	 */
+	}
+	
 	public User create(String nickname) {
 		User user = new User(nickname);
 		return userRepository.addUser(user);
 	}
-	/**
-	 * The method allows to obtain user.
-	 * @param nickname User's nickname. This is used to search the required user.
-	 * @return Required user instance.
-	 */
+
 	public User read(String nickname) {
 		return userRepository.getUser(nickname);
 	}
-	/**
-	 * The method allows updating an existing user.
-	 * @param oldNickname User's actual or old nickname. This is used to search the user to update.
-	 * @param user Updated user instance.
-	 * @return User update.
-	 */
+
 	public User update(String oldNickname, User user) {
 		return userRepository.updateUser(oldNickname, user);
 	}
-	/**
-	 * The method allows to delete user. 
-	 * @param nickname User's nickname. This is used to search the user to delete.
-	 */
+	
 	public boolean delete(String nickname) {
 		return userRepository.deleteUser(nickname);
 	}
